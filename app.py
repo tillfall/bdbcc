dea3d15aa4a01809eda80c7431ae67b00b113573
tillfall <tillfall@126.com>
2019-10-15 22:37:28 +0800

    realtime value

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ class RealtimeValue:
     
     @classmethod
     def get_one(cls, id):
-        url = url_template % id
+        url = cls.url_template % id
         logging.debug(url)
         resp = urlopen(url).read().decode('gbk')
         # var hq_str_s_sh000001="上证指数,2991.0459,-16.8375,-0.56,1553769,16953193";\n
