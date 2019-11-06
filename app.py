e2e88d36e1008673f455756ad7c7f6d300dfa1cc
tillfall <tillfall@126.com>
2019-11-07 06:20:52 +0800

    sh000016

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ app = Flask(__name__)
 class RealtimeValue:
     url_template = 'http://hq.sinajs.cn/list=s_%s'
     # 上证50,000016,中证500,000905，沪深300,000300，上证综指999999，深证成指399001，中小板399005，创业板399006
-    index_ids = 'sh000001,sh000905,sz399006,sz399001'
+    index_ids = 'sh000001,sh000016,sh000905,sz399006,sz399001'
     chart_tmplate = '''<tr><td>
     <img src="http://image.sinajs.cn/newchart/daily/n/%s.gif"></img>
     <img src="http://image.sinajs.cn/newchart/min/n/%s.gif"></img>
